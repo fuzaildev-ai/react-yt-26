db9bba8bd6520dda1966af7d10b06ac669f5521b
Mhd Fuzail <fuzaildev@gmail.com>
2026-04-07 13:25:13 +0500

    Added 02-components

---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,9 @@
+function Card() {
+    return (
+        <div className='card'>
+            <h1>MFuzail123</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi, nam?</p>
+        </div>
+    )
+}
+export default Card
