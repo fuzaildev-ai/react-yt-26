f8a4b747cda3f294543f425646b3184addef1816
Mhd Fuzail <fuzaildev@gmail.com>
2026-04-09 16:34:26 +0500

    install tailwind

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold underline bg-red-500 p-5 m-5 rounded-lg cursor-pointer hover:bg-red-600 transition-all duration-300'>App</div>
+  )
+}
+
+export default App
